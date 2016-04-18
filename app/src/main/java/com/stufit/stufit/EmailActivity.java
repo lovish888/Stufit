b89220422cb82e7b73850f04a9668e716db66943
lovish888 <lovish79214@gmail.com>
2016-04-18 19:42:29 +0530

    Email Activity complete and working properly.

--- app/src/main/java/com/stufit/stufit/EmailActivity.java
+++ app/src/main/java/com/stufit/stufit/EmailActivity.java
@@ -23,6 +23,7 @@ public class EmailActivity extends AppCompatActivity {
     public void createList(){
         String[] email_list = getResources().getStringArray(R.array.Email_to);
         emailList = (ListView) findViewById(R.id.listView);
+        //Creating an adapter to display items in ListView.
         ArrayAdapter<String> adapter = new ArrayAdapter(this,android.R.layout.simple_list_item_1,email_list);
         emailList.setAdapter(adapter);
     }
@@ -31,23 +32,33 @@ public class EmailActivity extends AppCompatActivity {
         emailList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                //Setting up implicit intent.
                 Intent email = new Intent(Intent.ACTION_SEND);
+                //Asking package manager for the apps which can send an Email.
                 email.setData(Uri.parse("mailto:"));
-                String to = getStrings(i);
-                int text = getEmailText(i);
-                email.putExtra(Intent.EXTRA_EMAIL, to);
+                String[] to = {getStrings(i)};
+                String text = getEmailText(i);
+                email.putExtra(Intent.EXTRA_EMAIL,to);
                 email.putExtra(Intent.EXTRA_TEXT,text);
+                //Setting the meme type.
+                email.setType("message/rfc822");
+                //Popup a chooser to select apps provided the package manager.
+                Intent chooser = Intent.createChooser(email, "Send Email");
+                startActivity(chooser);
             }
         });
     }
 
     public String getStrings(int i){
-        String[] emailLinks = {"sdangi724@gmail.com","shubhamv1204@gmail.com","niraj.lnmiit@gmail.com","vipul.viki@gmail.com","lostandfound.lnmiit@gmail.com" +
-                "lostandfound.lnmiit@gmail.com","sdgupta.gupta9@gmail.com","hostel@lnmiit.ac.in"};
+        String[] emailLinks = {"sdangi724@gmail.com","shubhamv1204@gmail.com","niraj.lnmiit@gmail.com","vipul.viki@gmail.com",
+                "lostandfound.lnmiit@gmail.com", "lostandfound.lnmiit@gmail.com","sdgupta.gupta9@gmail.com","hostel@lnmiit.ac.in"};
         return emailLinks[i];
     }
-    public int getEmailText(int i){
-        int[] emailText = {R.string.president_email,R.string.gsec_sports,R.string.gsec_cultural,R.string.gsec_scienceandtech,R.string.lostandfound,R.string.rfid_lost,R.string.vice_president,R.string.hostelaffairs};
+    public String getEmailText(int i){
+        String[] emailText = {getResources().getString(R.string.president_email),getResources().getString(R.string.gsec_sports)
+                ,getResources().getString(R.string.gsec_cultural),getResources().getString(R.string.gsec_scienceandtech)
+                ,getResources().getString(R.string.lostandfound),getResources().getString(R.string.rfid_lost)
+                ,getResources().getString(R.string.vice_president),getResources().getString(R.string.hostelaffairs)};
         return emailText[i];
     }
 }
